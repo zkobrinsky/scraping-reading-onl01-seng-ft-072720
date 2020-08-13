1aed30294150782556213d2c5e505913cff6f33c
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 14:40:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-doc = Nokogiri::HTML(html)
+# html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN")
