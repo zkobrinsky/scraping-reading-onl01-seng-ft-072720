a4657c83a9d8a910c7d22ea09a7bb1c055d4b3e9
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 14:47:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,8 @@ require 'open-uri'
 
 # html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".title-oE5vT4").text
+courses = doc.css(".title-oE5vT4")
+
+courses.each do |course|
+  puts course.text.strip
+end
